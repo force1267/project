599128984cdc9551618b86807cd8bc53835c73d0
MJ Asadi <MohammadJavad.Asadi@tapsi.cab>
2022-09-07 16:31:16 +0430

    Removed 3 main-page galleries from /api/galleries

--- src/api/gallery/controllers/gallery.js
+++ src/api/gallery/controllers/gallery.js
@@ -5,5 +5,18 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::gallery.gallery', ({ strapi }) => ({
+    async find(ctx) {
+        let { data, meta } = await super.find(ctx);
+        const main = await strapi.service('api::main-page.main-page').find({ populate: "NahjulBalagha, Quran, QandA" })
+        let { NahjulBalagha, Quran, QandA } = main
+        let p0, p1, p2
+        if(NahjulBalagha && NahjulBalagha.id) p0 = NahjulBalagha.id;
+        if(Quran && Quran.id) p1 = Quran.id;
+        if(QandA && QandA.id) p2 = QandA.id;
 
-module.exports = createCoreController('api::gallery.gallery');
+        data = data.filter(gallery => ![p0, p1, p2].includes(gallery.id))
+    
+        return { data, meta };
+      },
+}));
