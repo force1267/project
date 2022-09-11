aa988bc37333cdd151b17864af8e2d3f3930efe9
MJ Asadi <MohammadJavad.Asadi@tapsi.cab>
2022-09-11 16:46:19 +0430

    Bug Fix

--- src/api/gallery/controllers/gallery.js
+++ src/api/gallery/controllers/gallery.js
@@ -9,7 +9,9 @@ module.exports = createCoreController('api::gallery.gallery', ({ strapi }) => ({
     async find(ctx) {
         let { data, meta } = await super.find(ctx);
         const main = await strapi.service('api::main-page.main-page').find({ populate: "NahjulBalagha, Quran, QandA" })
-        let { NahjulBalagha, Quran, QandA } = main
+        let NahjulBalagha = main.NahjulBalagha
+        let Quran = main.Quran
+        let QandA = main.QandA
         let p0, p1, p2
         if(NahjulBalagha && NahjulBalagha.id) p0 = NahjulBalagha.id;
         if(Quran && Quran.id) p1 = Quran.id;
