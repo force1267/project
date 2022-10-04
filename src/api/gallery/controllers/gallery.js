b49e887f390a374306c274cc66d331c2dfb19015
MJ Asadi <MohammadJavad.Asadi@tapsi.cab>
2022-10-04 04:38:34 +0330

    Remove gallery logic

--- src/api/gallery/controllers/gallery.js
+++ src/api/gallery/controllers/gallery.js
@@ -5,21 +5,5 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
-module.exports = createCoreController('api::gallery.gallery', ({ strapi }) => ({
-    async find(ctx) {
-        let { data, meta } = await super.find(ctx);
-        const main = await strapi.service('api::main-page.main-page').find({ populate: "NahjulBalagha, Quran, QandA" })
-        if(main) {
-          let NahjulBalagha = main.NahjulBalagha;
-          let Quran = main.Quran;
-          let QandA = main.QandA;
-          let p0, p1, p2;
-          if(NahjulBalagha && NahjulBalagha.id) p0 = NahjulBalagha.id;
-          if(Quran && Quran.id) p1 = Quran.id;
-          if(QandA && QandA.id) p2 = QandA.id;
-          data = data.filter(gallery => ![p0, p1, p2].includes(gallery.id));
-        }
-    
-        return { data, meta };
-      },
-}));
+
+module.exports = createCoreController('api::gallery.gallery');
